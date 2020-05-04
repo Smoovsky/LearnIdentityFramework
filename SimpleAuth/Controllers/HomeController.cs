509224b5c2da4882c631f6aa85478f8d57999df3
Shaun Chen <Shang.Chen@globirdenergy.com.au>
2020-05-04 19:16:25 +1000

    1. rename

--- SimpleAuth/Controllers/HomeController.cs
+++ SimpleAuth/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LearnIdentityFramework.Controllers
+namespace SimpleAuth.Controllers
 {
     public class HomeController : Controller
     {
