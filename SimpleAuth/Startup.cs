a24b6b477a9be60f4285bb7de4572d00545ef5a3
Shaun Chen <Shang.Chen@globirdenergy.com.au>
2020-05-11 19:42:06 +1000

    1. add global auth

--- SimpleAuth/Startup.cs
+++ SimpleAuth/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -53,13 +54,16 @@ namespace LearnIdentityFramework
 
                 config.AddPolicy(
                     "Claim.DOB",
-                    policyBuilder => 
+                    policyBuilder =>
                     {
                         policyBuilder.AddRequirements(new CustomClaimRequirement(ClaimTypes.DateOfBirth));
                     });
             });
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(config =>
+            {
+                config.Filters.Add(new AuthorizeFilter());
+            });
 
             services.AddScoped<IAuthorizationHandler, CustomClaimAuthHandler>();
             // services.AddRazorPages();
