39f117c6c3e57f1245e4aa3274a8af0bbc840348
Shaun Chen <shang.chen@globirdenergy.com.au>
2020-05-19 23:00:30 +1000

    1. access resource remote resource using oauth token

--- OAuthClient/Controllers/HomeController.cs
+++ OAuthClient/Controllers/HomeController.cs
@@ -52,7 +52,7 @@ namespace OAuthClient.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult ServerSecretTest()
+        public IActionResult ServerSecretTest() 
         {
             var token = HttpContext.GetTokenAsync("access_token").Result;
 
